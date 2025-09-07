f30047f8a515e1b531e207ab34a318243aecef9d
NazariiKoropis”git config --global user.name NazariiKoropis” <nazarkoropisprogramming@gmail.comgit config --global user.email nazarkoropisprogramming@gmail.com>
2025-09-07 11:10:49 +0300

    lab32

---
+++ pr32/pr32.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+
+	double a;
+	double b;
+	double c;
+	double x;
+	double F;
+	std::cout << "Enter a, b, c, x: ";
+	std::cin >> a >> b >> c >> x;
+
+	if (x < 5 && b != 0)
+		F = a * std::pow((x + 7), 2) - b;
+	if( x > 5 && b == 0)
+		F = (x - c * a)/a*x;
+	if (!(x < 5 && b != 0) && !(x > 5 && b == 0))
+		F = x / c;
+
+	std::cout << "F = " << F << std::endl;
+
+
+	if (x < 5 && b != 0)
+		F = a * std::pow((x + 7), 2) - b;
+	else if (x > 5 && b == 0)
+		F = (x - c * a) / a * x;
+	else 
+		F = x / c;
+
+	std::cout << "F = " << F << std::endl;
+
+	return 0;
+}
